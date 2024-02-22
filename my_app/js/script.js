d3824ce00a9518ba250e62926ec5607e160abeae
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-22 15:57:56 +0100

    added index.php structure

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,11 +1,16 @@
 const { createApp } = Vue;
 
-const apiUri = 'http://localhost/PHP/php-todo-list-json/tasks';
+const endpoint = 'http://localhost/PHP/php-todo-list-json/tasks';
 
 const app = createApp({
     data: () => ({
         tasks: []
-    })
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+        })
+    }
 })
 
 app.mount('#app');
--- tasks/index.php
+++ tasks/index.php
@@ -1,28 +1,28 @@
 <?php
-
-$tasks = [
-    [
-        "id"=> 1,
-        "text"=> "HTML",
-        "done"=> false
-    ],
-    [
-        "id"=> 2,
-        "text"=> "CSS",
-        "done"=> false
-    ],
-    [
-        "id"=> 3,
-        "text"=> "Responsive design",
-        "done"=> false
-    ],
-    [
-        "id"=> 4,
-        "text"=> "Javascript",
-        "done"=> false
-    ]
-];
-
 header ('Content-Type: application/json');
 
-echo json_encode($tasks, true);
+$source_path = __DIR__ . '/../database/tasks.json';
+
+$json_data = file_get_contents($source_path);
+
+$tasks = $json_data;
+
+$new_task = $_POST['task'] ?? '';
+
+if ($new_task) {
+    
+    $tasks = json_decode($tasks);
+
+    if(in_array($new_task, $tasks)){
+        $tasks[] = $new_task;
+    } else {
+        echo json_encode('{"error": "Si é verificato un errore"}');
+    }
+
+    $tasks = json_encode($tasks);
+
+    file_put_contents($source_path, $tasks);
+}
+
+
+echo $tasks;
